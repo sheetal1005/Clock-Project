dd3e97a7503081f6fd71a7ce45a0ede4e2da80a1
sheetal95 <esa.sheetal95@gmail.com>
2023-04-12 16:09:47 +0530

    add files

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 
+
+
+currentDays();
+setInterval(updateTime, 1000);
+
 function updateTime() {
     let now = new Date();
     let seconds = now.getSeconds();
@@ -19,11 +24,29 @@ if (hours == 0) {
   mint = (minutes < 10) ? "0" + minutes : minutes;
   sec = (seconds < 10) ? "0" + seconds : seconds;
 
-
-    let formattedTime = `${hours}:${minutes}:${seconds < 10 ? '0' : ''}${seconds}:  ${session}`;
+    let formattedTime = `${hours}:${minutes}:${seconds < 10 ? '0' : ''}${seconds}  ${session}`;
     document.getElementById("clock").innerHTML = formattedTime;
-  }
+    let date = new Date();
+    let d= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+ 
+    let m = [ "January","February", "March","April","May","June","July","August" ,"September", "October", "November","December"];
+ 
+    let day = date.getDay();
+    let month = date.getMonth();
+    let year = date.getFullYear();
+    let  da = date.getDate();
+ 
+        let currentDate = d[day] + " , " + m[month] + "  " + year;
+      document.getElementsByClassName("date")[0].innerHTML = currentDate;  
+}
+ function currentDays(){
+
+    };
+
+  
 
 
 
-setInterval(updateTime, 1000);
+
+
+
